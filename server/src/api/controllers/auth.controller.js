810382ef466b0a967371d3f3ce3102082223e123
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-01-02 12:59:59 +0530

    Finish the user registration endpoint.
    add .env file template.
    create http requests to test the API endpoint.
    finish the user rehistration with user validation and password hashing.

--- server/src/api/controllers/auth.controller.js
+++ server/src/api/controllers/auth.controller.js
@@ -1,11 +1,29 @@
-function registeruser(req, res) {}
+const { validateUser } = require("../validations/user");
+const { checkUserExists, createUser } = require("../services/userTable");
+
+async function registerUser(req, res) {
+  // check if the provided informations validate the user schema
+  const { error } = validateUser(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const userExists = await checkUserExists(req.body.username, req.body.email);
+  if (userExists.error) {
+    return res.status(409).send(userExists.error);
+  }
+
+  // create the user
+  const user = await createUser(req.body);
+  res.json(user);
+}
 
 function login(req, res) {}
 
 function logout(req, res) {}
 
 module.exports = {
-  registeruser,
+  registerUser,
   login,
   logout,
 };
---
+++ server/src/api/services/userTable.js
@@ -0,0 +1,62 @@
+const { Op } = require("sequelize");
+const bcrypt = require("bcrypt");
+const { UserAuth } = require("../models");
+const { User } = require("../models");
+
+const hashSalt = 10;
+
+async function checkUserExists(username, email) {
+  // check if wither email or username exists in the system
+  const user = await UserAuth.findOne({
+    where: {
+      userName: username,
+    },
+  });
+
+  // return appropriate error message basec on existence of the username and email
+  if (user) {
+    if (user.email == email)
+      return { error: "User with this email already exists", value: user };
+    else if (user.userName == username)
+      return { error: "Use with this username already exists", value: user };
+    return { error: "User already exists", value: user };
+  }
+  return { value: null };
+}
+
+// save the validate user to the database
+async function createUser(data) {
+  // first create the hash password from the plain text password
+  const hash = await bcrypt.hash(data.password, hashSalt);
+  // create a instance of the User
+  const user = User.build({
+    firstName: data.firstname,
+    lastName: data.lastname,
+    location: data.location || null,
+    Bio: data.bio || null,
+    profilePic: data.profile || null,
+    personalSite: data.personalsite || null,
+  });
+  await user.save();
+
+  // create a instance of UserAuth model
+  const userAuth = UserAuth.build({
+    userId: user.userId,
+    userName: data.username,
+    password: hash,
+    email: data.email,
+  });
+
+  await userAuth.save();
+
+  return {
+    username: userAuth.userName,
+    email: userAuth.email,
+    fullName: user.fullName,
+  };
+}
+
+module.exports = {
+  checkUserExists,
+  createUser,
+};
