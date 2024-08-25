e88ea070931f55bfa15bdd197554d29f8fc66df0
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-08-26 03:31:17 +0530

    chore: Update package.json and App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { AuthContext } from "./contexts/auth.context";
 import { SearchContextProvider } from "./contexts/search.context";
 import { PopupContextProvider } from "./contexts/popup.context";
